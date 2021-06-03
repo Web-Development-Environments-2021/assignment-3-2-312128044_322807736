45d6788cc430dccdc57ab71731a9ca1b369c18b6
Daniel <zamanski@post.bgu.ac.il>
2021-06-03 12:50:34 +0300

    tried getting team information by partial name, didnt test yet

--- project/routes/teams.js
+++ project/routes/teams.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const DButils = require("./utils/DButils");
 const players_utils = require("./utils/players_utils");
+const team_utils = require("./utils/team_utils");
 
 router.get("/teamFullDetails/:teamId", async (req, res, next) => {
   let team_details = [];
@@ -15,5 +16,20 @@ router.get("/teamFullDetails/:teamId", async (req, res, next) => {
     next(error);
   }
 });
+
+router.get("/teamFullDetails/search/:teamName", async (req, res, next) => {
+  let team_details = [];
+
+  try {
+    const team_ids = await team_utils.getTeamIDByName(req.params.teamName);
+    team_details = await team_utils.getTeamsInfo(team_ids);
+    //we should keep implementing team page.....
+    res.send(team_details);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
 
 module.exports = router;
--- project/routes/utils/team_utils.js
+++ project/routes/utils/team_utils.js
@@ -9,7 +9,34 @@ async function getTeamNameByID(team_id)
         },
       });
     const team_name = team.data.data.name;
-    console.log(team_name);
+    
     return(team_name);
 }
+async function getTeamIDByName(team_name)
+{
+    const team = await axios.get(`${api_domain}/teams/search/${team_name}`, {
+        params: {
+          api_token: process.env.api_token,
+        },
+      });
+    const team_id = team.data.data.id;
+    
+    return(team_id);
+}
+async function getTeamsByPartialName(team_name)
+{
+    let partial = new RegExp(team_name,'i');
+    const teams = await axios.get(`${api_domain}/teams/search/${partial}`, {
+        params: {
+          api_token: process.env.api_token,
+        },
+      });
+    
+    
+    return(teams);
+}
+
+
 exports.getTeamNameByID = getTeamNameByID;
+exports.getTeamIDByName = getTeamIDByName;
+exports.getTeamsByPartialName = getTeamsByPartialName;
